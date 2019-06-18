cb0f18617b38145fd142d12644a5dbd50deee73f
MichaelObvious <michele.palese42@outlook.it>
2019-06-18 15:24:21 +0200

    added premake

---
+++ premake5.lua
@@ -0,0 +1,40 @@
+project "GLFW"
+    kind "StaticLib"
+    language "C"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    
+    files {
+        "include/GLFW/glfw3.h",
+        "include/GLFW/glfw3native.h",
+        "src/glfw_config.h",
+        "src/context.c",
+        "src/init.c",
+        "src/input.c",
+        "src/monitor.c",
+        "src/vulkan.c",
+        "src/window.c"
+    }
+
+    filter "system:linux"
+        systemversion "latest"
+        staticruntime "On"
+        pic "On"
+
+        files{
+            "src/x11_init.c",
+            "src/x11_monitor.c",
+            "src/x11_window.c",
+            "src/xkb_unicode.c",
+            "src/posix_time.c",
+            "src/posix_thread.c",
+            "src/glx_context.c",
+            "src/egl_context.c",
+            "src/osmesa_context.c",
+            "src/linux_joystick.c"
+        }
+
+        defines {
+            "_GLFW_X11"
+        }
