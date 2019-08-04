c8e654f0d57a5519a138cdacce1daf51cd6a739e
MichaelObvious <michele.palese42@outlook.it>
2019-08-04 21:08:19 +0200

    added windows support

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,7 @@
 project "GLFW"
     kind "StaticLib"
     language "C"
+    cdialect "C11"
 
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -19,10 +20,10 @@ project "GLFW"
 
     filter "system:linux"
         systemversion "latest"
-        staticruntime "On"
-        pic "On"
+        staticruntime "on"
+        pic "on"
 
-        files{
+        files {
             "src/x11_init.c",
             "src/x11_monitor.c",
             "src/x11_window.c",
@@ -38,3 +39,32 @@ project "GLFW"
         defines {
             "_GLFW_X11"
         }
+    
+    filter "system:windows"
+        --buildoptions { "-std=c11", "-lgdi32" }
+        systemversion "latest"
+        staticruntime "on"
+        
+        files {
+            "src/win32_init.c",
+            "src/win32_joystick.c",
+            "src/win32_monitor.c",
+            "src/win32_time.c",
+            "src/win32_thread.c",
+            "src/win32_window.c",
+            "src/wgl_context.c",
+            "src/egl_context.c",
+            "src/osmesa_context.c"
+        }
+
+        links {
+            "gdi32"
+        }
+
+        defines { 
+            "_GLFW_WIN32",
+            "_CRT_SECURE_NO_WARNINGS"
+		}
+    
+    filter { "system:windows", "configurations:Release" }
+        buildoptions "/MT"
